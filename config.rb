98159300fe3fc417a4d4e51443766b132e0a8792
Allison Law <hsallisonlaw@gmail.com>
2014-07-31 00:03:03 -0400

    Updated Gemfile and config.rb

--- config.rb
+++ config.rb
@@ -1,5 +1,5 @@
+require 'compass/import-once/activate'
 # Require any additional compass plugins here.
-require "breakpoint"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -8,14 +8,19 @@ sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
+# Require any additional compass plugins installed on your system.
+require 'sassy-buttons'
+require 'breakpoint'
+require "rgbapng"
+
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
