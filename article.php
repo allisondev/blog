2405b90446d4cd3f1eae607b6502b8893e2beef4
Allison Law <hsallisonlaw@gmail.com>
2014-07-30 23:37:27 -0400

    Fixed screen.css link in article.php, removed unused files

--- article.php
+++ article.php
@@ -33,7 +33,7 @@ $row = mysql_fetch_array($result);
 
       ?>
     </title>
-    <link rel="stylesheet" type="text/css" href="stylesheets/style.css" />
+    <link rel="stylesheet" type="text/css" href="stylesheets/screen.css" />
     <script type="text/javascript"
     src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> 
     <script type="text/javascript" src="scripts/blog.js"></script>
