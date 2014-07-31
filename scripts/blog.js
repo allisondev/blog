f4318d6d00d2472a532b486d3272ae5b9156c29b
Allison Law <hsallisonlaw@gmail.com>
2014-07-31 01:28:59 -0400

    Added debugging console.logs

--- scripts/blog.js
+++ scripts/blog.js
@@ -18,7 +18,10 @@ $(document).ready(function(){
     // Clicking the header will slide up current page
     $(document).on('click', 'header h1 a', function(){
 
+      console.log('Clicked header h1 a.');
+      console.log($(this));
       $(this).slideDown(3000);
+
 
     });
 });
